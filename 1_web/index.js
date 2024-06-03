5f59508c7be2122b97a320ddc6857ccfb09e0eec
PiyushGhavghave <piyushghavghave@gmail.com>
2024-06-04 04:24:31 +0530

    new commit

--- 1_web/index.js
+++ 1_web/index.js
@@ -1,43 +1,56 @@
-// console.log(`Hello`);
-// console.log(`Piyush`);
+console.log(`Hello`);
+console.log(`Piyush`);
 
 // window.alert("This is an alert");
 // window.alert(`I like pizza`);
 
 // Add text using js
+document.getElementById("myH1").textContent = "Hello";
+document.getElementById("myp").textContent = "I like pizza";
 
-// document.getElementById("myH1").textContent = "Hello";
-// document.getElementById("myp").textContent = "I like pizza";
+// ==============Variable ===========
+// Numbers
+let age=100;
+let cgpa= 10.36;
+console.log(typeof age,'and ',typeof cgpa)
 
-const mynums = [1,2,3]
+// string 
+let username = "Piyush";
+console.log(typeof username)
 
-const total = mynums.reduce(function (acc,curr){
-    console.log(`acc: ${acc} and currval: ${curr}`);
-    return acc + curr;
-},0);
-console.log(total);
+// Boolean 
+let isOnline = false;
+console.log(typeof isOnline);
 
-const mytotal =mynums.reduce( (acc,curr) => acc+curr,0)
-console.log(mytotal);
+// =============arithmatic operator==========
+
+let num =30;
+num += 3;
+num -= 2;
+num *= 4;
+num /= 2;
+num **=3;  //power
+num %= 2;  //modulo
+console.log(num+=2);
+
+//=========== User input ===================
+
+// let num1 = parseInt(window.prompt("enter value of a "));
+// let num2 = parseInt(window.prompt("enter value of b "));
+// window.alert(`a+b = ${num1+num2}`)
+
+let msg ;
+document.getElementById("mysub").onclick = function(){
+    msg = document.getElementById("myinput").value;
+    console.log(msg);
+}
 
 
-const mycart = [
-    {
-        itemname : "js course",
-        price : 999
-    },
-    {
-        itemname : "py course",
-        price : 1000
-    },
-    {
-        itemname : "cpp course",
-        price : 3000
-    }
-]
 
-const cartTotal = mycart.reduce((acc,curr) => acc+curr.price,0);
-console.log(cartTotal);
+
+
+
+
 
 
 
